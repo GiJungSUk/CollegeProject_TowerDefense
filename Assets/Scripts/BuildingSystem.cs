a0f372e484fa37c71383e9dc3a508a1e8affd17c
GiJungSUk <134478458+GiJungSUk@users.noreply.github.com>
2024-09-12 11:15:33 +0900

    24.09.11
    빌딩시스템 대형 건물 설정

--- Assets/Scripts/BuildingSystem.cs
+++ Assets/Scripts/BuildingSystem.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class BuildingSystem : MonoBehaviour
 {
-    public static int selectedButtonID;   // 선택된 버튼의 고유 번호를 저장할 변수
+    public static int selectedButtonID;   // 선택된 버튼의 고유 번호를 저장할 변수 0~9 소형건물 10 ~ 대형건물
     [SerializeField]
     GameObject[] building; // 프리펩
     List<GameObject> building_Objcet = new List<GameObject>(); //실제로 생성된 프리펩들
@@ -36,29 +36,68 @@ public class BuildingSystem : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
+                bool canBuild;
                 Vector3 buildPos = new Vector3(hit.collider.transform.position.x, 0.1f, hit.collider.transform.position.z); //건설할 위치
+                Collider[] hitcollider = Physics.OverlapSphere(hit.collider.transform.position, 1f, 1 << 6);
 
-                if (hit.collider.tag == "CanBuild") // 건설 가능일 때
+                canBuild = JudgeBuild(hit , hitcollider);
+
+                if (canBuild) // 건설 가능일 때
                 {
                     ChangeMat(building_Objcet[selectedButtonID], Color.green);
                     building_Objcet[selectedButtonID].transform.position = buildPos;
 
                     if (Input.GetMouseButtonDown(0)) // 클릭 했을 때 그 자리에 건설
                     {
-                        hit.collider.tag = "CantBuild";
+                        if(selectedButtonID < 10) // 소형 건물이면 그 자리만 건설불가로 만들고
+                            hit.collider.tag = "CantBuild";
+                        else // 대형 건물이면 3x3 지형을 건설 불가로 만든다
+                        {
+                            foreach (Collider collider in hitcollider)
+                            {
+                                collider.tag = "CantBuild";
+                            }
+                        }
                         Instantiate(building[selectedButtonID], buildPos , Quaternion.identity);
                         startBuild = false;
-                        building_Objcet[selectedButtonID].transform.position = originalPos;
+                        building_Objcet[selectedButtonID].transform.position = originalPos; 
                     }
                 }
 
-                else if (hit.collider.tag == "CantBuild") // 건설 불가능할 때
+                else if (!canBuild) // 건설 불가능할 때
                 {
                     ChangeMat(building_Objcet[selectedButtonID], Color.red);
                     building_Objcet[selectedButtonID].transform.position = buildPos;
                 }
             }
         }
+    }
+
+    private bool JudgeBuild(RaycastHit hit , Collider[] hitcollider)
+    {
+        bool returnFlag = false;
+
+        if(selectedButtonID >= 10) // 10 이상이면 대형 건물
+        {
+            foreach (Collider collider in hitcollider)
+            {
+                if (collider.gameObject.tag == "CantBuild")
+                    return returnFlag;
+            }
+            returnFlag = true;
+            return returnFlag;
+        }
+        else // 10 이하면 소형 건물
+        {
+            if (hit.collider.tag == "CanBuild")
+            {
+                returnFlag = true;
+                return returnFlag;
+            }
+            else
+                return returnFlag;
+        }
+       
     }
 
     private void ChangeMat(GameObject objcet, Color color)
