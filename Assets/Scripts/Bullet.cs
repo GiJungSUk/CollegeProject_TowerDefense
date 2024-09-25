513e3fb5c7b78cae0ae7cfedb1b0f0e612e8ea72
GiJungSUk <134478458+GiJungSUk@users.noreply.github.com>
2024-09-25 20:44:19 +0900

    240925
    타워 공격 피격 활성화
    체력시스템 구현

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -12,13 +12,36 @@ public class Bullet : MonoBehaviour
     void FixedUpdate()
     {
         if (target == null)
+        {
+            Destroy(gameObject);
             return;
+        }
+            
         BulletMove();
+        BulletDamaged();
+    }
+    private void Update()
+    {
+        
     }
 
     private void BulletMove()
     {
         transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position);
         transform.position += (target.transform.position - transform.position ) * bulletSpeed * Time.deltaTime;
+    }
+
+    private void BulletDamaged()
+    {
+        Collider[] col = Physics.OverlapSphere(gameObject.transform.position, 0.2f, 1 << 7);
+
+        foreach (Collider enemy in col)
+        {
+            if (enemy != null)
+            {
+                enemy.GetComponent<HpSystem>().Hp -= bulletDamage;
+                Destroy(gameObject);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/HpSystem.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class HpSystem : MonoBehaviour
+{
+    [SerializeField]
+    float hp = 100f;
+    public float Hp
+    {
+        get
+        {
+            return hp;
+        }
+        set
+        {
+            if(hp > 0)
+            {
+                hp = value;
+            }
+            else
+            {
+                Die();
+            }
+        }
+    }
+    private void Start()
+    {
+        Hp = hp;
+    }
+
+    private void Die()
+    {
+            Destroy(gameObject);
+    }
+}
